5f4c26bb16b3a0a68207d133b9d458edbe6c52ef
wsheaven <ws.heaven@gmail.com>
2024-06-22 12:59:47 -0600

    Complete use axios for api crud functions for the posts. Using local json-server

--- src/PostPage.js
+++ src/PostPage.js
@@ -11,7 +11,8 @@ const PostPage = ({ posts, handleDelete }) => {
                         <h2>{post.title}</h2>
                         <p className="postDate">{post.datetime}</p>
                         <p className="postBody">{post.body}</p>
-                        <button onClick={() => handleDelete(post.id)}>
+                        <Link to={`/post/edit/${post.id}`}><button>Edit Post</button></Link>
+                        <button className="deleteButton" onClick={() => handleDelete(post.id)}>
                             Delete Post
                         </button>
                     </>
