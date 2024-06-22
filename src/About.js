1d3175fd3ad715494c5f7b26628638e421d21d6a
wsheaven <ws.heaven@gmail.com>
2024-06-22 12:05:06 -0600

    Set up blog with all functionality using local hardcoded data

---
+++ src/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <main className='About'>
+            <h2>About</h2>
+            <p style={{ marginTop: "1rem" }}>This is a blog that I am building in react to practice and learn more about it.</p>
+        </main>
+    )
+}
+
+export default About
--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter as router, Route, Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-      <Route path="/" Component={App} />
+      <Routes>
+      <Route path="/*" element={<App/>} />
+      </Routes>
     </Router>
   </React.StrictMode>
 );
