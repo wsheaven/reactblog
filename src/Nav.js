4df0a90fea6026b21eb207f34bd6430f5364a87f
wsheaven <ws.heaven@gmail.com>
2024-06-24 14:45:07 -0600

    refactor to use context instead of drilling down props. Could refactor farther as everything is in the context

--- src/Nav.js
+++ src/Nav.js
@@ -1,26 +1,36 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import DataContext from "./context/DataContext";
 
-const Nav = ({ search, setSearch }) => {
-    return (
-        <nav className='Nav'>
-            <form className='searchForm' onSubmit={(e) => e.preventDefault()}>
-                <label htmlFor="search">Search Posts</label>
-                <input
-                    type="text"
-                    placeholder='Search Posts'
-                    id="search"
-                    value={search}
-                    onChange={(e) => setSearch(e.target.value)}
-                />
-            </form>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="post">Post</Link></li>
-                <li><Link to="about">About</Link></li>
-            </ul>
-        </nav>
-    )
-}
+const Nav = () => {
+  const { search, setSearch } = useContext(DataContext);
 
-export default Nav
+  return (
+    <nav className="Nav">
+      <form className="searchForm" onSubmit={(e) => e.preventDefault()}>
+        <label htmlFor="search">Search Posts</label>
+        <input
+          type="text"
+          placeholder="Search Posts"
+          id="search"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </form>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="post">Post</Link>
+        </li>
+        <li>
+          <Link to="about">About</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
